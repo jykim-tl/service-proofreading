fd6cbbe83665bbb1e5f55633aa510520d5d70358
jykim-tl <jy.kim@topialive.co.kr>
2024-09-13 15:02:40 +0900

    L1 included

--- commentGuides.py
+++ commentGuides.py
@@ -6,7 +6,8 @@ def getCommentGuide(subjectName:str, levelName:str):
         "V3":'3rd',
         "O1":'4th',
         "O2":'4th',
-        "O3":'4th'
+        "O3":'4th',
+        "L1":'5th'
     } 
 
     commentPromptMapBySubjectName ={
@@ -261,11 +262,22 @@ def getCommentGuide(subjectName:str, levelName:str):
                     - Sentence Count: Ensure that the student has met the minimum requirement of 20 sentences. If they haven't, suggest ways to expand their ideas, add supporting details, or incorporate more research to meet this requirement.
                 - The overall comment should be one paragraph, without bullet points, at least but no longer than 15 sentences, and should start with a compliment. The general aim should be to encourage the student while providing specific, actionable advice for improvement. Do not use too many “but”s as it may discourage the student. Provide examples for improvement as well.
             ''',
+            "L1":'''
+                - Please provide constructive feedback on a student's short speech, written using the provided template. The student is at an AR 5 (Lexile 850) level and has written a speech that includes at least 16 sentences: one topic sentence, one conclusion sentence, and three body paragraphs. The feedback should be written in a friendly and encouraging tone, focusing on the following areas:
+                    - Organization: Comment on the clarity and structure of the speech, ensuring the topic sentence, body paragraphs, and conclusion sentence are effectively organized.
+                    - Main Idea and Supporting Details: Evaluate how well the student presents the main idea and supports it with relevant details in the body paragraphs. Recommend that the student includes details such as problems, benefits, effects or side effects, necessity, harms, alternatives, etc., about the given resolution.
+                    - Use of Outside Sources: Assess the student's use of outside source evidence to support their argument. Offer suggestions on how to better integrate research or provide additional evidence if needed.
+                    - Vocabulary: Assess the student's choice of vocabulary, offering suggestions for improvement if necessary.
+                    - Sentence Structure: Evaluate the variety and complexity of the student's sentence structures. Encourage them to use different types of sentences (simple, compound, complex) to enhance the flow and clarity of their speech. l Paraphrasing: If relevant, mention the effectiveness of paraphrasing within the speech.
+                    - Sentence Count: Ensure that the student has met the minimum requirement of 20 sentences. If they haven't, suggest ways to expand their ideas, add supporting details, or incorporate more research to meet this requirement.
+                - The overall comment should be one paragraph, without bullet points, at least but no longer than 15 sentences, and should start with a compliment. The general aim should be to encourage the student while providing specific, actionable advice for improvement. Do not use too many “but”s as it may discourage the student. Provide examples for improvement as well.
+            ''',
         },
         "WRITING":f'''
         - It was written by an elementary school student who is learning English as a foreign language. The student's English level is equivalent to that of a {gradeMapByLevelName[levelName.upper()]} grader in the U.S.
         - Please use a friendly tone and mention specific parts of the essay, highlighting three key positive aspects. Also, point out the areas that need improvement with model examples and explain why these aspects are problematic, using two key terms.
         ''',
+        
         "SPEAKING&VOCABULARY":f'''
         - It was written by an elementary school student who is learning English as a foreign language. The student's English level is equivalent to that of a {gradeMapByLevelName[levelName.upper()]} grader in the U.S.
         - Please use a friendly tone and mention specific parts of the essay, highlighting three key positive aspects. Also, point out the areas that need improvement with model examples and explain why these aspects are problematic, using two key terms.
